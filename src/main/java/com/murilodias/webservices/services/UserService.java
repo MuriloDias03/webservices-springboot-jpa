115e687a2a2f772f9f53f6a16c56d4ca94a8a00d
Murilo C Dias <murilocristovaodias@gmail.com>
2024-12-21 13:27:00 -0300

    Criação do service layer e do componente de registro

---
+++ src/main/java/com/murilodias/webservices/services/UserService.java
@@ -0,0 +1,25 @@
+package com.murilodias.webservices.services;
+
+import com.murilodias.webservices.entities.User;
+import com.murilodias.webservices.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> finAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
