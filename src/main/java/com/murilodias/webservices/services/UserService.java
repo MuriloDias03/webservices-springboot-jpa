2975e10895dea71703fef18167d51c11dd0b8f40
Murilo C Dias <murilocristovaodias@gmail.com>
2024-12-26 17:49:27 -0300

    Exceção do método delete

--- src/main/java/com/murilodias/webservices/services/UserService.java
+++ src/main/java/com/murilodias/webservices/services/UserService.java
@@ -2,8 +2,11 @@ package com.murilodias.webservices.services;
 
 import com.murilodias.webservices.entities.User;
 import com.murilodias.webservices.repositories.UserRepository;
+import com.murilodias.webservices.services.exceptions.DatabaseException;
 import com.murilodias.webservices.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
